f0b448a9d154fab713498b7792cb82d19e461491
DukiChoi <cbcc1234@naver.com>
2022-01-03 05:16:06 +0900

    220103 05:16

--- app/src/main/java/com/example/myapplication/MainActivity4.java
+++ app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -1,14 +1,27 @@
 package com.example.myapplication;
+
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity4 extends AppCompatActivity {
 
+    Spinner spinner;
+    String[] items = {"아이템0","아이템1","아이템2","아이템3","아이템4"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main4);
+
+        //여기서부터 스피너 시작
+
+    }
+
+    public void onButton9Clicked(View view) {
+        finish();
     }
 }
