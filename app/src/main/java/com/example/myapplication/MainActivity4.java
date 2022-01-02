fc8ae328b224c1f84464ad7f9640774f5bad7c9f
cbcc1 <cbcc1234@naver.com>
2022-01-03 01:18:05 +0900

    220103 01:17

--- app/src/main/java/com/example/myapplication/MainActivity4.java
+++ app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
-
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity4 extends AppCompatActivity {
@@ -9,6 +8,7 @@ public class MainActivity4 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main4);
     }
 }
