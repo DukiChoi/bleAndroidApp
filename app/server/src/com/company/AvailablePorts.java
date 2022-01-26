a3e5982770b3d460f9dd821752d519ffbd8c2d5a
DukiChoi <cbcc1234@naver.com>
2022-01-26 16:14:04 +0900

    22-01-26

---
+++ app/server/src/com/company/AvailablePorts.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.net.Socket;
+
+public class AvailablePorts {
+
+    public static boolean availablePort(String host, int port)
+    {
+        boolean result = false;
+
+        try {
+            (new Socket(host, port)).close();
+
+            result = true;
+        }
+        catch(Exception e) {
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args){
+
+        boolean portCheck = availablePort("127.0.0.1",5050);
+        if(portCheck){
+            System.out.println("port available");
+        }
+        else
+        {
+            System.out.println("port unavailable");
+        }
+    }
+}
