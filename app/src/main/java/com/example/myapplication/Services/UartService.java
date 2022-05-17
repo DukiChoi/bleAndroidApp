3dcea302c62526207c44ab028e271574ed8a4c74
byungkeun <cbcc1234>
2022-05-18 00:02:23 +0900

    Changed RxChar with TxChar

--- app/src/main/java/com/example/myapplication/Services/UartService.java
+++ app/src/main/java/com/example/myapplication/Services/UartService.java
@@ -76,7 +76,7 @@ public class UartService extends Service {
     
     public static final UUID TX_POWER_UUID = UUID.fromString("00001804-0000-1000-8000-00805f9b34fb");
     public static final UUID TX_POWER_LEVEL_UUID = UUID.fromString("00002a07-0000-1000-8000-00805f9b34fb");
-    public static final UUID CCCD = UUID.fromString("00009999-0000-1000-8000-00805f9b34fb");
+    public static final UUID CCCD = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
     public static final UUID FIRMWARE_REVISON_UUID = UUID.fromString("00002a26-0000-1000-8000-00805f9b34fb");
     public static final UUID DIS_UUID = UUID.fromString("0000180a-0000-1000-8000-00805f9b34fb");
     public static final UUID RX_SERVICE_UUID = UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
